d7d9d9825656c4d2fcaa9485c44a27ce3554bcb2
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2017-01-28 17:17:16 -0600

    Initial structure

---
+++ Surveys.Droid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Surveys.Droid
+{
+    [Activity(Label = "Surveys.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
