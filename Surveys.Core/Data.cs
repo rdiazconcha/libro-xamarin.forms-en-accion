1d6b9e548ccf0663c5bf4f43f7a8cb26ce876658
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2017-02-04 23:28:30 -0600

    Implementación de todo el Capítulo 5

---
+++ Surveys.Core/Data.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace Surveys.Core
+{
+    public class Data : NotificationObject
+    {
+        private ObservableCollection<Survey> surveys;
+
+        public ObservableCollection<Survey> Surveys
+        {
+            get
+            {
+                return surveys;
+            }
+            set
+            {
+                if (surveys == value)
+                {
+                    return;
+                }
+                surveys = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Survey selectedSurvey;
+
+        public Survey SelectedSurvey
+        {
+            get
+            {
+                return selectedSurvey;
+            }
+            set
+            {
+                if (selectedSurvey == value)
+                {
+                    return;
+                }
+                selectedSurvey = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Data()
+        {
+            Surveys = new ObservableCollection<Survey>();
+
+            MessagingCenter.Subscribe<ContentPage, Survey>(this, Messages.NewSurveyComplete,
+                (sender, args) => { Surveys.Add(args); });
+        }
+    }
+}
--- Surveys.Core/SurveysView.xaml.cs
+++ Surveys.Core/SurveysView.xaml.cs
@@ -8,14 +8,6 @@ namespace Surveys.Core
         public SurveysView()
         {
             InitializeComponent();
-
-            MessagingCenter.Subscribe<ContentPage, Survey>(this, Messages.NewSurveyComplete, (sender, args) =>
-            {
-                SurveysPanel.Children.Add(new Label()
-                {
-                    Text = args.ToString()
-                });
-            });
         }
 
         private async void AddSurveyButton_Clicked(object sender, EventArgs e)
