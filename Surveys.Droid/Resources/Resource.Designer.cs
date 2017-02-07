f67f96d29d4bc7d5cfbaf72f51bf69452af857c4
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2017-02-07 07:47:18 -0600

    Se implementan las barras de herramientas de las páginas y se substituyen los botones actuales.

--- Surveys.Droid/Resources/Resource.Designer.cs
+++ Surveys.Droid/Resources/Resource.Designer.cs
@@ -2201,7 +2201,13 @@ namespace Surveys.Droid
 			public const int mr_ic_play_light = 2130837664;
 			
 			// aapt resource value: 0x7f0200a1
-			public const int notification_template_icon_bg = 2130837665;
+			public const int newcapture = 2130837665;
+			
+			// aapt resource value: 0x7f0200a3
+			public const int notification_template_icon_bg = 2130837667;
+			
+			// aapt resource value: 0x7f0200a2
+			public const int ok = 2130837666;
 			
 			static Drawable()
 			{
